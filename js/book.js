308fc8e66dc553f1edb1a6bc6950cb1c8b94047d
Ursula MorenoVanderLaan <ursula.moreno-vanderlaan@techtonic.com>
2018-10-02 16:35:00 -0600

    addBook function working

--- js/book.js
+++ js/book.js
@@ -5,7 +5,10 @@ function Book(title, author, numPages, pubDate){
   this.pubDate = pubDate;
 };
 
-
  Book.prototype.editBook = function(oBook) {
 
  };
+
+ var book1 = new Book("Where the Wild Things Are", "Maurice Sendak", 31, "September 9, 1978");
+
+ var book2 = new Book("The Subtle Knife", "Philip Pullman", 358, "December 21, 1997");
--- js/engine.js
+++ js/engine.js
@@ -3,22 +3,55 @@ function Library(){
 };
 
 Library.prototype.addBook = function (book) {
-
+  //push book to bookshelf array O(N)
+  for (i = 0; i < this.bookShelf.length; i++){
+     if(book.title === this.bookShelf[i].title) {
+        alert("Why are you buying two copies of the same book??");
+        return false;
+      }
+  }
+  this.bookShelf.push(book);
+  return true;
 };
 
 Library.prototype.removeBookByTitle = function (title) {
-
+  //loop through objects, if title key has a value that matches entered title, delete object from bookshelf
+  for (i = 0; i < this.bookShelf.length; i++){
+     if(book.title === this.bookShelf[i].title) {
+        
+      }
+  }
+  this.bookShelf.push(book);
+  return true;
 };
 
 Library.prototype.removeBookByAuthor = function (author) {
+  // loop through objects, if author key has a value that matches entered author, delete object from bookshelf
+};
+
+Library.prototype.getRandomBook = function () {
+  //use math method random to select an index number, pull object and return associated book
+};
+
+Library.prototype.getBookByTitle = function (title) {
 
 };
 
-Library.prototype.addBook = function () {
+Library.prototype.getBookByAuthor = function (authorname) {
 
 };
 
+Library.prototype.addBooks = function (books) {
 
+};
+
+Library.prototype.getAuthors = function () {
+
+};
+
+Library.prototype.getRandomAuthorName = function () {
+
+};
 
 document.addEventListener("DOMContentLoaded", function(e){
   window.gLibrary = new Library();
