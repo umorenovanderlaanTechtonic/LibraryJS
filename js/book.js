633fae7211182d2536a9c8c3e4f197455c581adf
Ursula MorenoVanderLaan <ursula.moreno-vanderlaan@techtonic.com>
2018-10-05 11:26:43 -0600

    code is commented, user input is normalized, library is more reliably updated upon edit, remove, and add.

--- js/book.js
+++ js/book.js
@@ -5,7 +5,10 @@ function Book(title, author, numPages, pubDate){
   this.pubDate = new Date(pubDate.toString());
 };
 
+
+//DONE:0 reset storage after editing a book
  Book.prototype.editBook = function(oBook) {
+   //for each potential input, checks if there is input, and if there is, overwrites the value with new value
    if (oBook.title != undefined) {
      this.title = oBook.title;
    }
@@ -18,6 +21,8 @@ function Book(title, author, numPages, pubDate){
    if (oBook.pubDate != undefined) {
      this.pubDate = oBook.pubDate;
    }
+   //updates library after the object has been edited
+   gLibrary.createStorage();
    return this;
  };
 
@@ -25,6 +30,6 @@ var book1 = new Book("Where the Wild Things Are", "Maurice Sendak", 31, "Septemb
 var book2 = new Book("The Subtle Knife", "Philip Pullman", 358, "December 21, 1994");
 var book3 = new Book("The Golden Compass", "Philip Pullman", 297, "June 15, 1992");
 var book4 = new Book("The Amber Spyglass", "Philip Pullman", 403, "Aug 7, 1997");
- var edit1 = {title:'The Subtle Knife', author: "Phillip Pullman"};
+var edit1 = {title:'The Subtle Knife', author: "Phillip Pullman"};
 
- var books1 = [book1, book2];
+var books1 = [book1, book2];
--- js/engine.js
+++ js/engine.js
@@ -1,10 +1,14 @@
-//Okay I've learned more about iffes and this makes sense now.
+
 (function() {
+  //creates undedined var of singleton
   var singleton;
+  //defines Library
   Library = function () {
-    if (singleton != undefined) {
+    //if singleton exists, return it
+    if (singleton) {
       return singleton;
     }
+  //singleton equals the returned value, and the bookshelf is an array.
   singleton = this;
   this.bookShelf = [];
 };
@@ -12,24 +16,29 @@
 
 
 Library.prototype.addBook = function (book) {
-  //push book to bookshelf array O(N)
+  //push book to bookshelf array-O(N), next line normalizes user input
+  title = book.title.trim().toLowerCase();
   for (var i = 0; i < this.bookShelf.length; i++){
-    if(book.title === this.bookShelf[i].title) {
+  //the complexity of the comparison is necessary to normalize both user input and the way titles have been entered into the database
+    if(title === this.bookShelf[i].title.trim().toLowerCase()) {
         alert("Why are you buying two copies of the same book??");
         return false;
     }
   }
   this.bookShelf.push(book);
+//this updated the library, and is also called when books are edited and removed
   gLibrary.createStorage();
   return true;
 };
 
 Library.prototype.removeBookByTitle = function (title) {
   //loop through objects, if title key has a value that matches entered title, delete object from bookshelf
+  title = title.trim().toLowerCase();
   for (var i = 0; i < this.bookShelf.length; i++){
-    if(title === this.bookShelf[i].title) {
+    if(title === this.bookShelf[i].title.trim().toLowerCase()) {
         this.bookShelf.pop(this.bookShelf[i]);
         console.log("Book Removed");
+        gLibrary.createStorage();
         return true;
     }
   }
@@ -39,10 +48,12 @@ Library.prototype.removeBookByTitle = function (title) {
 
 Library.prototype.removeBookByAuthor = function (author) {
   // loop through objects, if author key has a value that matches entered author, delete object from bookshelf
+  author = author.trim().toLowerCase();
   for (var i = 0; i < this.bookShelf.length; i++){
-     if(author === this.bookShelf[i].author) {
+     if(author === this.bookShelf[i].author.trim().toLowerCase()) {
         this.bookShelf.pop(this.bookShelf[i]);
         console.log("Book Removed");
+        gLibrary.createStorage();
         return true;
       }
   }
@@ -51,9 +62,11 @@ Library.prototype.removeBookByAuthor = function (author) {
 };
 
 Library.prototype.getRandomBook = function () {
+  //checks to make sure there are books in the library in the first place
   if(this.bookShelf.length === 0){
     return null;
   }
+  //if there are books, generates a random number within the limits of the bookshelf array length and returns the book at that index
   else{
     var min = 0;
     var max = Math.floor(this.bookShelf.length);
@@ -63,10 +76,10 @@ Library.prototype.getRandomBook = function () {
 };
 
 Library.prototype.getBookByTitle = function (title) {
-  title = title.toLowerCase();
+  title = title.trim().toLowerCase();
   var filteredA = [];
   for (var i = 0; i < this.bookShelf.length; i++){
-    if(this.bookShelf[i].title.toLowerCase().indexOf(title.toLowerCase()) > -1) {
+    if(this.bookShelf[i].title.trim().toLowerCase().indexOf(title) > -1) {
         filteredA.push(this.bookShelf[i]);
     }
   }
@@ -75,10 +88,10 @@ Library.prototype.getBookByTitle = function (title) {
 
 
 Library.prototype.getBookByAuthor = function (authorName) {
-  authorName = authorName.toLowerCase();
+  authorName = authorName.trim().toLowerCase();
   var filteredA = [];
   for (var i = 0; i < this.bookShelf.length; i++){
-    if(this.bookShelf[i].author.toLowerCase().indexOf(authorName.toLowerCase()) > -1) {
+    if(this.bookShelf[i].author.trim().toLowerCase().indexOf(authorName) > -1) {
         filteredA.push(this.bookShelf[i]);
     }
   }
@@ -86,31 +99,41 @@ Library.prototype.getBookByAuthor = function (authorName) {
 };
 
 Library.prototype.addBooks = function (books) {
+  //sets booksAdded as a counter
   var booksAdded = 0;
+  //runs through all books that are being passed, evaluates based on addBook function
   for (var i = 0; i < books.length; i++) {
     if (this.addBook(books[i])) {
+    //increments counter as books return true
     booksAdded++;
     }
   }
+  //shows the counter
   return booksAdded;
 };
 
 Library.prototype.getAuthors = function () {
+  //creates two empty arrays for manipulating.
   var rawA = [];
   var uniqA = [];
+  //pushes authors indiscriminately to rawA
   for (var i = 0; i < this.bookShelf.length; i++) {
     rawA.push(this.bookShelf[i].author);
   }
+  //filters the rawA to eliminate duplicate authors
   uniqA = rawA.filter(function(value,index,self){
     return self.indexOf(value) === index;
   });
+  //returns
   return uniqA;
 };
 
 Library.prototype.getRandomAuthorName = function () {
+  //checks if bookshelf is empty
   if(this.bookShelf.length === 0){
     return null;
   }
+  //duplicates RandomBook logic, but returns author of the book rather than book object.
   else{
     var min = 0;
     var max = Math.floor(this.bookShelf.length);
@@ -138,28 +161,33 @@ Library.prototype.search = function(entry) {
   return uniqResults;
   };
 
-
-// TO SAVE LIBRARY STATE, YOU MUST CREATE STORAGE FOR LIBRARY AGAIN, BUT THIS HAS BEEN AUTOMATED FOR ADD BOOK FUNCTION ONLY NECESSARY AFTER EDITING?? TEST THIS. //
 Library.prototype.createStorage = function(){
   localStorage.setItem('gLibrary',JSON.stringify(this.bookShelf));
   return console.log("Library State Saved.");
 };
 
 Library.prototype.getStorage = function(){
+  //creates empty array
   var bookObjs = [];
+  //gets local storage and stores it as a var
   var jsonObj = JSON.parse(localStorage.getItem("gLibrary"));
+  //runs through each item in jsonObj and recreates them as a book object, then pushes re-prototyped books to book Objs array
   for (var i = 0; i < jsonObj.length; i++) {
     bookObjs.push(new Book(jsonObj[i].title , jsonObj[i].author , jsonObj[i].numPages , jsonObj[i].pubDate));
   }
+  //returns bookified array (this is what gets defined as the bookshelf when the page loads)
   return bookObjs;
 };
 
 document.addEventListener("DOMContentLoaded", function(e){
+  //on load, sets library according to library function at top of doc
   window.gLibrary = new Library();
 
+//if local storage has anything in it, the saved library information will be loaded using getStorage function
   if (window.localStorage.length > 0) {
     console.log("Loading Last Library State.");
     window.gLibrary.bookShelf = gLibrary.getStorage()}
+    //if there is nothing in local storage, a new Library will be created, a set list of books will be loaded, and a copy will be stored in local storage
     else {
       console.log("Creating New Library.");
       gLibrary.addBooks(books1);
