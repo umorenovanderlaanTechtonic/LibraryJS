749835661905cb6e8edfe46cd3b2bae70fc9ba72
Ursula MorenoVanderLaan <ursula.moreno-vanderlaan@techtonic.com>
2018-10-05 08:39:02 -0600

    added books for tests

--- js/book.js
+++ js/book.js
@@ -21,9 +21,10 @@ function Book(title, author, numPages, pubDate){
    return this;
  };
 
- var book1 = new Book("Where the Wild Things Are", "Maurice Sendak", 31, "September 9, 1978");
- var book2 = new Book("The Subtle Knife", "Philip Pullman", 358, "December 21, 1997");
- var book3 = new Book("The Golden Compass", "Philip Pullman", 297, "June 15, 1992");
+var book1 = new Book("Where the Wild Things Are", "Maurice Sendak", 31, "September 9, 1978");
+var book2 = new Book("The Subtle Knife", "Philip Pullman", 358, "December 21, 1994");
+var book3 = new Book("The Golden Compass", "Philip Pullman", 297, "June 15, 1992");
+var book4 = new Book("The Amber Spyglass", "Philip Pullman", 403, "Aug 7, 1997");
  var edit1 = {title:'The Subtle Knife', author: "Phillip Pullman"};
 
  var books1 = [book1, book2];
